9b17886995286bfcd9ee60b70bd3f85418138b31
Thomas Drage <20510505@student.uwa.edu.au>
2020-03-23 23:27:38 +0800

    alternative join

--- geodinverse.for
+++ geodinverse.for
@@ -25,3 +25,28 @@
 
         end subroutine geo_dist
 
+
+        function to_radian(degree) result (rad)
+          ! degrees to radians
+          real,intent(in) :: degree
+          real, parameter :: deg_to_rad = atan(1.0)/45 ! exploit intrinsic atan to generate pi/180 runtime constant
+          real :: rad
+ 
+          rad = degree*deg_to_rad
+        end function to_radian
+ 
+        subroutine haversine(deglat1,deglon1,deglat2,deglon2, dist)
+          ! great circle distance -- adapted from Matlab 
+          real,intent(in) :: deglat1,deglon1,deglat2,deglon2
+          real :: a,c,dlat,dlon,lat1,lat2
+          real, intent(out) :: dist
+          real,parameter :: radius = 6370.433 
+ 
+          dlat = to_radian(deglat2-deglat1)
+          dlon = to_radian(deglon2-deglon1)
+          lat1 = to_radian(deglat1)
+          lat2 = to_radian(deglat2)
+          a = (sin(dlat/2))**2 + cos(lat1)*cos(lat2)*(sin(dlon/2))**2
+          c = 2*asin(sqrt(a))
+          dist = radius*c
+        end subroutine haversine
